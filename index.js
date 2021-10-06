3cc8899128b8a8bab866f5e92dde5face80cba25
pjwhite08 <pamelajeanwhite@gmail.com>
2021-10-05 20:02:22 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let express= require("express")
+require("dotenv").config()
+let app = express()
+
+let PORT = process.env.PORT
+
+app.get("/", function(req, res){
+    res.send("Helloooooo Heroku App")
+})
+
+app.listen(PORT, function(){
+    console.log("Leeeeeet's go! on port: ", PORT)
+})
